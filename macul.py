b17a88c0158976f952e3e5f0d78faeda81681dff
Keda87 <adiyatmubarak@gmail.com>
2019-11-23 10:17:37 +0700

    fix: blocking coroutine

--- macul.py
+++ macul.py
@@ -1,50 +1,74 @@
 import asyncio
 import aioredis
-import functools
+import attr
+import logging
 import json
 import os
 import sys
 import time
 import uvloop
 
+from functools import wraps
 from aioredis.errors import ConnectionClosedError
 
 
-QUEUE = os.environ.get('QUEUE_NAME')
-REDIS_HOST = os.environ.get('REDIS_HOST')
-REDIS_PASS = os.environ.get('REDIS_PASS')
-REDIS_PORT = os.environ.get('REDIS_PORT')
-REDIS_DB = os.environ.get('REDIS_DB')
-
 PID = os.getpid()
-QUEUE_TASK = f'macul:{QUEUE}:task'
-QUEUE_FAIL = f'macul:{QUEUE}:fail'
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s,%(msecs)d %(levelname)s: %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+@attr.s
+class Message:
+    id = attr.ib()
+    event = attr.ib()
+    body = attr.ib()
 
 
 class Macul:
 
-    def __init__(self):
+    def __init__(self, event_name, namespace='macul'):
+        self.redis = None
+        self.namespace = namespace
+        self.event_name = event_name
+        self.queue_name = 'default'
+
+    @property
+    def queue_task_name(self):
+        return f'{self.namespace}:{self.queue_name}:task'
+
+    @property
+    def queue_fail_name(self):
+        return f'{self.namespace}:{self.queue_name}:fail'
+
+    def init_redis(self, host='127.0.0.1', port='6379', db=0, password=None):
         self.redis = aioredis.create_redis_pool(
-            f'redis://{REDIS_HOST}:{REDIS_PORT}',
-            password=REDIS_PASS,
-            db=int(REDIS_DB),
+            f'redis://{host}:{port}',
+            db=db,
+            password=password,
         )
 
-    def consumer(self):
+    def consumer(self, queue_name=None):
         def wrapper(func):
+            @wraps(func)
             async def wrapped(*args):
-                print(f'Worker {PID} is listening on "{QUEUE_TASK}"')
-                redis_connection = await self.redis
+                if queue_name is not None:
+                    self.queue_name = queue_name
+                print(f'Worker {PID} is listening on "{self.queue_task_name}"')
+                redis_conn = await self.redis
                 while True:
-                    _, data = await conn.brpop(QUEUE_TASK)
+                    _, data = await redis_conn.brpop(self.queue_task_name)
                     data = json.loads(data.decode('utf8'))
                     try:
-                        await func(data)
+                        if data['event'] == self.event_name:
+                            asyncio.create_task(func(data))
                     except KeyError:
-                        print('invalid payload')
+                        logging.error('invalid payload')
                     except Exception:
                         data = json.dumps(data)
-                        redis.lpush(QUEUE_FAIL)
+                        redis.lpush(self.queue_fail_name, data)
+                        logging.info('Failed event moved to fail queue')
             return wrapped
         return wrapper
 
@@ -55,7 +79,7 @@ class Macul:
             asyncio.ensure_future(func())
             event_loop.run_forever()
         except ConnectionClosedError:
-            print('Connection closed...')
+            logging.error('Connection closed')
         except KeyboardInterrupt:
             sys.exit(1)
         finally:
